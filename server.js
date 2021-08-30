6061d9a747cb24a743d4399abbae46d68092810d
gustavohdev <g_a93@outlook.com>
2021-08-30 13:35:06 -0300

    api working, mongo db connected, creating trip POST, list of trips GET

--- server.js
+++ server.js
@@ -1,14 +1,61 @@
-const express = require("express");
-const mongo = require("mongodb").MongoClient;
+const express = require("express")
+const mongo = require("mongodb").MongoClient
+const url = "mongodb://localhost:27017"
 
-const app = express();
+const app = express()
+
+app.use(express.json())
+
+let db, trips, expenses
+
+mongo.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    db = client.db("tripcost")
+    trips = db.collection("trips")
+    expenses = db.collection("expenses")
+  }
+)
+
 
 app.post("/trip", (req, res) => {
     /* */
+
+    const name = req.body.name;
+    trips.insertOne({ name: name }, (err, result) => {
+
+        if(err){
+            console.error(err)
+            res.status(500).json({err:err})
+            return
+        }
+        console.log(result)
+        res.status(200).json(({ok:true}))
+
+    });
 });
+
 app.get("/trips", (req, res) => {
     /* */
+    trips.find().toArray((err, items) => {
+
+        if(err){
+            console.log(err)
+            res.status(500).json({err:err})
+            return
+        }
+        res.status(200).json({ trips:items })
+    })
 });
+
 app.post("/expense", (req, res) => {
     /* */
 });
